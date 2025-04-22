426a71778f844459f8d725b53b53000df5da3b09
Nilesh Wankhede <114302591+nileshwankhede03@users.noreply.github.com>
2025-04-22 14:08:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ const port = 3000;
 
 // Sample TODOs
 const todos = [
-  { id: 1, title: "Buy groceries", completed: false },
-  { id: 2, title: "Walk the dog", completed: true },
-  { id: 3, title: "Read a book", completed: false },
-  { id: 4, title: "Write some code", completed: true },
-  { id: 5, title: "Clean the room", completed: false },
+  { id: 1, title: "Buy groceries", description : "this is a desc of Buy groceries", completed: false },
+  { id: 2, title: "Walk the dog", description : "this is a desc of Walk the dog", completed: true },
+  { id: 3, title: "Read a book", description : "this is a desc of Read a book", completed: false },
+  { id: 4, title: "Write some code",description : "this is a desc of Write some code ",  completed: true },
+  { id: 5, title: "Clean the room", description : "this is a desc of Clean the room ", completed: false },
+  { id: 6, title: "Clean the Hall", description : "this is a desc of Clean the hall", completed: false },
+  { id: 7, title: "Clean the kitchen", description : "this is a desc of Clean the kitchen.", completed: false },
+  { id: 8, title: "Clean the bed", description : "this is a desc of Clean the bed.", completed: false },
+  { id: 9, title: "Clean the bike", description : "this is a desc of Clean the Bike", completed: false },
+  { id: 10, title: "Clean the scooty",description : "this is a desc of Clean the scooty",  completed: false },
+  { id: 11, title: "Clean the clothes",description : "this is a desc of Clean the clothes",  completed: false },
+  { id: 12, title: "Clean the PG", description : "this is a desc of Clean the PG.", completed: false },
+  { id: 13, title: "Study DSA", description : "life is sorted with this ", completed: false },
+  { id: 14, title: "Study DBMS with SQL", description : "explore the concepts of a DBMS", completed: false },
+  { id: 15, title: "Rivision of WEB-dev", description : "learn React.js", completed: false },
+  { id: 16, title: "So ja mere bhai",description : "this is a desc of sleeping1",  completed: false },
 ];
 
 // Utility to get random TODO
