916605719f1f1534646294f31666c18030f73fe6
Nilesh Wankhede <114302591+nileshwankhede03@users.noreply.github.com>
2025-04-22 17:55:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(cors()); // <== Enable CORS for all origins
+app.use(express.json()); // Parses JSON body
 
 // Sample TODOs
 const todos = [
